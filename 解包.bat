e630fd9bad93a8eca74fc08b56d27f3610268323
Sdator <250740270@qq.com>
2019-12-04 20:15:57 +0800

    first commit

---
+++ 解包.bat
@@ -0,0 +1,14 @@
+@echo off
+
+pushd %~dp0
+set app=
+
+
+for /r %%i in (*borpak.exe) do set "app=%%i"
+
+for /r %%i in (*.pak) do echo ÊÇ·ñÒª½âÑ¹£¿"%app%" "%%i"  && pause >nul && "%app%" "%%i"
+
+
+
+
+exit
